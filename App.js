2bc97ac50fe1ea1edddfe79cacf88d6651914631
lekkyj <lekanjayeola0@gmail.com>
2025-05-06 15:49:08 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import PlayersList from './components/PlayersList';
+
+function App() {
+  return (
+    <div>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
