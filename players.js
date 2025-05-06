a51549d417a5b529743e3b7b647a63d58a2319fb
lekkyj <lekanjayeola0@gmail.com>
2025-05-06 15:47:15 +0100

    Create players.js

---
+++ players.js
@@ -0,0 +1,37 @@
+// src/players.js
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/89/Lionel_Messi_20180626.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 25,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/88/Kylian_Mbapp%C3%A9_2019.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 39,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norway",
+    jerseyNumber: 9,
+    age: 23,
+    image: "https://upload.wikimedia.org/wikipedia/commons/2/26/Erling_Haaland_2020.jpg"
+  }
+];
+
+export default players;
